a51a9801041a8917548ab80729a4b90a8fd0c094
Thomas Binu <thomas.binu@gistec.com>
2016-04-19 20:31:33 +0400

    Added dj_database

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -17,7 +17,6 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
